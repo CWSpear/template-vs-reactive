1c21e8292531269f77f114da6577159fc48d7302
Cameron Spear <cameronspear@gmail.com>
2018-07-14 20:15:14 -0700

    Rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to template-forms-proof!');
+    expect(page.getParagraphText()).toEqual('Welcome to template-vs-reactive!');
   });
 });
